1bab7dafc9e101051dedec6f32e379ffe5c837d5
Jose Carmona <josshue85@gmail.com>
2024-11-02 03:52:47 -0300

    Requerimiento 1 listo

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,9 @@
+// Requerimiento 1
+// Transformar la siguiente declaración de función a una función de expresión
+// function example(a, b, c){
+//     return a + b +c
+// }
+// Función de expresión
+example = function(a, b, c){
+    return a + b + c
+}
