cb5a3c6db82bfef85bbc8227e6a67a1ac70821b7
Jose Carmona <josshue85@gmail.com>
2024-11-02 03:58:53 -0300

    Requerimiento 2 listo

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+// Requerimiento 2
+// Transformar la siguiente declaración de función a una function arrow
+// let suma = function(a, b){
+//     return a + b
+// }
+// Arrow function
+let suma = (a, b) => a + b
