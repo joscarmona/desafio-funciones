8473f79c03c2b15b46b90711e574d8e260c6de17
Jose Carmona <josshue85@gmail.com>
2024-11-03 01:13:58 -0300

    Punto 4 del requerimiento 3 listo

--- assets/js/script_keydown.js
+++ assets/js/script_keydown.js
@@ -17,12 +17,63 @@ function pintar(elementId, color = 'white'){
     const element = document.getElementById(elementId)
     element.style.backgroundColor = color
 }
+/* *********************************************************** */
 
-// Evento para escuchar las teclas que se presionan y se ejecutará la función en correspondencia con la tecla que se presiona
+/* *********************************************************** */
+/* ************************* PUNTO 4 ************************* */
+/* *********************************************************** */
+// variable que guardará el id del elemento creado
+let newElementId
+// Función para agregar un elemento web dado
+// Por defecto, se define un valor al parámetro webelement, en este caso un elemento web div que es lo solicitado en el punto 4
+function addElement(elementId, webelement = 'div'){
+
+    // Obtenemos el elemento contenedor por su ID, dentro del cual se desea crear o guardar un elemento web dado
+    const container = document.querySelector('#' + elementId)
+    
+    // Creamos un elemento web, en este caso un div
+    const newElement = document.createElement(webelement)
+
+    // Dimensiones del div teniendo en cuenta lo solicitado
+    newElement.style.width = '200px'
+    newElement.style.height = '200px'
+
+    // Al nuevo elemento se le asigna un ID
+     newElement.id = idGenerator()
+     newElementId = newElement.id
+     console.log(newElementId)
+
+    // Asignamos un texto al nuevo elemento
+    newElement.textContent = 'Elemento agregado ' + idnumber
+
+    // Agregamos el nuevo elemento al contenedor
+    container.appendChild(newElement)
+
+    
+}
+// se inicializa variable
+let idnumber = 0
+// Función generar ID para los elementos web
+let idGenerator = function(){
+    // Incrementa el valor en 1 
+    idnumber += 1
+    // Retorna el ID del nuevo elemento
+    return 'new-element-' + idnumber
+}
+/* *********************************************************** */
+
+/* *************************************** PUNTO 3 y 4 ****************************************/
+// Evento para escuchar las teclas que se presionan y se ejecutará la función en correspondencia con la tecla que se presiona de acuerdo a los solicitado en los puntos 3 y 4 respectivamente
+/* PUNTO 3: */
 // 'a': color rosado
 // 's': color naranjo
 // 'd': color celeste
+/* PUNTO 4: */
+// 'q': se crea un div nuevo de color morado con las mismas dimensiones que el div del punto 3
+// 'w': se crea un div nuevo de color gris con las mismas dimensiones que el div del punto 3
+// 'e': se crea un div nuevo de color café con las mismas dimensiones que el div del punto 3
 document.addEventListener('keydown', function (event) {
+    /* ************ PUNTO 1 ************ */
     // Si es presionada la tecla 'a'
     if (event.key === 'a') {
         color = 'pink'
@@ -38,45 +89,22 @@ document.addEventListener('keydown', function (event) {
         color = 'skyblue'
         pintar('key',color)
     }
+
+    /* ************ PUNTO 4 ************ */
     // Si es presionada la tecla 'q'
     else if(event.key === 'q'){
-        addElement()
-        pintar('new-element','purple')
+        addElement('container')
+        pintar(newElementId,'purple')
     }
     // Si es presionada la tecla 'w'
     else if(event.key === 'w'){
-        addElement()
-        pintar('new-element','gray')
+        addElement('container')
+        pintar(newElementId,'gray')
     }
     // Si es presionada la tecla 'e'
     else if(event.key === 'e'){
-        addElement()
-        pintar('new-element','brown')
+        addElement('container')
+        pintar(newElementId,'brown')
     }
     })
-/* *********************************************************** */
-
-/* *********************************************************** */
-/* ************************* PUNTO 4 ************************* */
-/* *********************************************************** */
-//Función para agregar un elemento web
-function addElement(){
-    // Obtenemos el elemento contenedor por su ID
-    const container = document.getElementById('container')
-    // contenedor = document.querySelector('#contenedor')
-    
-    // Creamos un elemento <div>
-    // const nuevoElemento = document.createElement('div')
-    const newElement = document.createElement('div')
-
-    // Asignamos un texto al nuevo elemento
-    newElement.textContent = 'Elemento agregado'
-
-    // Agregamos el nuevo elemento al contenedor
-    container.appendChild(newElement)
-
-    newElement.style.width = '200px'
-    newElement.style.height = '200px'
-    newElement.id = 'new-element'
-}
 /* *********************************************************** */
