b92b78fbc66bdbd61ecc56e3a490181aca93e7d2
Jose Carmona <josshue85@gmail.com>
2024-11-03 00:13:28 -0300

    Punto 3 del requerimiento 3 listo

--- assets/js/script_keydown.js
+++ assets/js/script_keydown.js
@@ -4,41 +4,61 @@
 /* **************************** Requerimiento 3: punto 3 y punto 4 ************************** */
 /* ****************************************************************************************** */
 
-//Variable global para guardar un color
-var color
+/* *********************************************************** */
+/* ************************* PUNTO 3 ************************* */
+/* *********************************************************** */
+// Variable global para guardar un color dado
+let color
 
-// Agrego un evento para escuchar las teclas presionadas
+// Función para pintar un determinado elemento wed
+// Se define el parámetro color con un valor por defecto, en este caso 'white'
+function pintar(elementId, color = 'white'){
+    // Selección del elemento web pasado como parámetro, en este caso un div
+    const element = document.getElementById(elementId)
+    element.style.backgroundColor = color
+}
+
+// Evento para escuchar las teclas que se presionan y se ejecutará la función en correspondencia con la tecla que se presiona
+// 'a': color rosado
+// 's': color naranjo
+// 'd': color celeste
 document.addEventListener('keydown', function (event) {
+    // Si es presionada la tecla 'a'
     if (event.key === 'a') {
         color = 'pink'
         pintar('key', color)
-    } else if (event.key === 's') {
+    }
+    // Si es presionada la tecla 's'
+    else if (event.key === 's') {
         color = 'orange'
         pintar('key', color)
-    } else if (event.key === 'd'){
+    }
+    // Si es presionada la tecla 'd'
+    else if (event.key === 'd'){
         color = 'skyblue'
         pintar('key',color)
-    } else if(event.key === 'q'){
+    }
+    // Si es presionada la tecla 'q'
+    else if(event.key === 'q'){
         addElement()
         pintar('new-element','purple')
-    } else if(event.key === 'w'){
+    }
+    // Si es presionada la tecla 'w'
+    else if(event.key === 'w'){
         addElement()
         pintar('new-element','gray')
-    } else if(event.key === 'e'){
+    }
+    // Si es presionada la tecla 'e'
+    else if(event.key === 'e'){
         addElement()
         pintar('new-element','brown')
     }
     })
+/* *********************************************************** */
 
-
-
-//Función para pintar un determinado elemento wed
-function pintar(elementId, color = 'white'){
-    // Selección del elemento web div
-    const divElement = document.getElementById(elementId)
-    divElement.style.backgroundColor = color
-}
-
+/* *********************************************************** */
+/* ************************* PUNTO 4 ************************* */
+/* *********************************************************** */
 //Función para agregar un elemento web
 function addElement(){
     // Obtenemos el elemento contenedor por su ID
@@ -59,3 +79,4 @@ function addElement(){
     newElement.style.height = '200px'
     newElement.id = 'new-element'
 }
+/* *********************************************************** */
