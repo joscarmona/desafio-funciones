b91b4244fcb159083d3431533504f0f413d2e87a
Jose Carmona <josshue85@gmail.com>
2024-11-02 23:36:03 -0300

    Algunos ajustes en los códigos correspondientes a los requerimientos 1 y 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,20 @@
 /* ******************************* Requerimiento 2.1, 2.2 y 2.3****************************** */
 /* ****************************************************************************************** */
 
-// Función que modifica color de fondo de determinado elemento web
-function pintar(color = 'green'){
-    ele.style.backgroundColor = color
+
+
+// Función que modifica color de fondo de un elemento web dado
+// Se define un valor por defecto al parámetro color, en este caso 'green'
+function pintar(elementId, color = 'green'){
+    // Selección de elemento que se recibe por parámetro
+    const element = document.querySelector('#' + elementId)
+    element.style.backgroundColor = color
 }
 
 // Selección del elemento al cual se le modificará el color de fondo
-const ele = document.getElementById("ele1")
+const ele = document.getElementById('ele1')
 // Se ejecuta la función pintar() cuando ocurre el evento click encima del elemento web seleccionado
 ele.addEventListener("click", function(){
-    pintar('yellow')
+    pintar('ele1', 'yellow')
 })
 
---
+++ assets/js/script_keydown.js
@@ -0,0 +1,60 @@
+/* *************************** Dashboard - JavaScript para la web *************************** */
+/* ********************************** Desafío 3 - Funciones ********************************* */
+/* *************************** Requerimiento 3, los últimos puntos ************************** */
+/* ****************************************************************************************** */
+
+//Variable global para guardar un color
+var color
+
+// Agrego un evento para escuchar las teclas presionadas
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+        color = 'pink'
+        pintar('key', color)
+    } else if (event.key === 's') {
+        color = 'orange'
+        pintar('key', color)
+    } else if (event.key === 'd'){
+        color = 'skyblue'
+        pintar('key',color)
+    } else if(event.key === 'q'){
+        addElement()
+        pintar('new-element','purple')
+    } else if(event.key === 'w'){
+        addElement()
+        pintar('new-element','gray')
+    } else if(event.key === 'e'){
+        addElement()
+        pintar('new-element','brown')
+    }
+    })
+
+
+
+//Función para pintar un determinado elemento wed
+function pintar(elementId, color = 'white'){
+    // Selección del elemento web div
+    const divElement = document.getElementById(elementId)
+    divElement.style.backgroundColor = color
+}
+
+//Función para agregar un elemento web
+function addElement(){
+    // Obtenemos el elemento contenedor por su ID
+    const container = document.getElementById('container')
+    // contenedor = document.querySelector('#contenedor')
+    
+    // Creamos un elemento <div>
+    // const nuevoElemento = document.createElement('div')
+    const newElement = document.createElement('div')
+
+    // Asignamos un texto al nuevo elemento
+    newElement.textContent = 'Elemento agregado'
+
+    // Agregamos el nuevo elemento al contenedor
+    container.appendChild(newElement)
+
+    newElement.style.width = '200px'
+    newElement.style.height = '200px'
+    newElement.id = 'new-element'
+}
